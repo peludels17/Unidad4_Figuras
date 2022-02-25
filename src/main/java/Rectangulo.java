ad8306d696c830f3c3f7675213a0a3a683947b16
anxo <anxorodriguezmendez@gmail.com>
2022-02-25 22:08:29 +0100

    Creacion del metodo obtenerPerimetro

--- src/main/java/Rectangulo.java
+++ src/main/java/Rectangulo.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Rectangulo {
+public class Rectangulo {
+
+    private double lado1;
+    private double lado2;
+
+    public Rectangulo(double l1, double l2){
+        lado1 = l1;
+        lado2 = l2;
+    }
+
+    public double obtenerArea(){
+        return lado1 * lado2;
+    }
+
+    public double obtenerPerimentro(){
+        return (lado1*2)+(lado2*2);
+    }
+
 }
